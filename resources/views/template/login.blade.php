a14825c5e36c1e6c22851780a53a0596afb70aa2
ahmadaspy <aspy98gokils@gmail.com>
2021-07-14 02:36:13 +0800

    template home n login

---
+++ resources/views/template/login.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <link rel="icon" href="{{ asset('assets/logo/logo.png') }}">
+        <!-- Custom fonts for this template-->
+        <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+        <!-- Custom styles for this template-->
+        <link href="{{ asset('assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
+        <style>
+            .fill img {
+                object-fit: cover;
+                width: 50px;
+                height: 500px;
+            }
+            #icon{
+                height: 30px;
+                width: 30px;
+            }
+            .fa-vials{
+                color:#4FB286;
+            }
+            .sidebar-brand-text{
+                color: #4FB286;
+            }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+            <a class="navbar-brand d-flex align-items-center justify-content-center" href="/">
+                <div class="sidebar-brand-icon">
+                  {{-- <img id="icon" src="fa" alt=""> --}}
+                  <i class="fas fa-vials"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">SPLTV</div>
+            </a>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="" id="Register">
+                        <span>Register</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/login" id="Login">
+                        <span>Login</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="container">
+            @yield('alert')
+            <div class="row justify-content-center">
+
+                <div class="col-xl-10 col-lg-12 col-md-9">
+
+                  <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                      <!-- Nested Row within Card Body -->
+                      <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block" id="resize">
+                          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                              <div class="carousel-inner">
+                                <div class="carousel-item active fill">
+                                  <img src="{{ asset('img/1.png') }}" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item fill">
+                                  <img src="{{ asset('img/2.png') }}" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item fill">
+                                  <img src="{{ asset('img/3.png') }}" class="d-block w-100" alt="...">
+                                </div>
+                              </div>
+                              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
+                          </div>
+                        </div>
+                        <div class="col-lg-6">
+                          <div class="p-5">
+                            <div class="text-center">
+                              <h1 class="h4 text-gray-900 mb-4">@yield('lognregisjudul')</h1>
+                            </div>
+                            @yield('form')
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+
+              </div>
+        </div>
+        <!-- Bootstrap core JavaScript-->
+        <script src="{{ asset('assets/vendor/jquery/jquery.min.js')}}"></script>
+        <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+        <!-- Core plugin JavaScript-->
+        <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+        <!-- Custom scripts for all pages-->
+        <script src="{{ asset('assets/js/sb-admin-2.min.js')}}"></script>
+
+        <!-- Page level plugins -->
+        <script src="{{ asset('assets/vendor/chart.js/Chart.min.js')}}"></script>
+
+        <!-- Page level custom scripts -->
+        <script src="{{ asset('assets/js/demo/chart-area-demo.js')}}"></script>
+        <script src="{{ asset('assets/js/demo/chart-pie-demo.js')}}"></script>
+        @yield('costumescript')
+    </body>
+</html>
+
+
+
---
+++ resources/views/template/template.blade.php
@@ -0,0 +1,214 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>@yield('title')</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}} rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> --}}
+    <link href="{{asset('assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="{{asset('mycostumecss.css')}}"> --}}
+
+    @yield('script')
+
+</head>
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav sidebar sidebar-dark accordion" style="background-color: #4FB286" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-vials"></i>
+                </div>
+                <div class="sidebar-brand-text mx-2">SPLTV</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="">
+                    <i class="fas fa-campground"></i>
+                    <span>Panduan</span></a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="">
+                    <i class="fas fa-chalkboard-teacher"></i>
+                    <span>KI | KD</span></a>
+            </li>
+            {{-- nav item collapse --}}
+            <hr class="sidebar-divider">
+            <div class="sidebar-heading">
+                Mata Pelajaran
+            </div>
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne"
+                    aria-expanded="true" aria-controls="collapseOne">
+                    <i class="fas fa-book"></i>
+                    <span>1. Sistem Persamaan Linear Dua Variabel</span>
+                </a>
+                <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Materi :</h6>
+                        <a class="collapse-item" href="buttons.html">1.1 Menyelesaikan <br>Persamaan <br> Dengan Metode Grafik</a>
+                        <a class="collapse-item" href="cards.html">1.2 Menyelesaikan <br>Persamaan<br> Linear Secara Aljabar</a>
+                        <a class="collapse-item" href="cards.html">Kuis</a>
+                    </div>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-book"></i>
+                    <span>2. Sistem Persamaan Linear Tiga Variabel</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Materi :</h6>
+                        <a class="collapse-item" href="buttons.html">2.1 Menyelesaikan<br>Sistem Persamaan<br>Linear Tiga Variabel</a>
+                        <a class="collapse-item" href="cards.html">2.2 Masalah Yang <br>Melibatkan Persamaan<br>Linear Tiga Variabel</a>
+                        <a class="collapse-item" href="cards.html">Kuis</a>
+                    </div>
+                </div>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="">
+                    <i class="fas fa-book"></i>
+                    <span>Evaluasi</span></a>
+            </li>
+            <hr class="sidebar-divider">
+            <li class="nav-item ">
+                <a class="nav-link" href="">
+                    <i class="fas fa-address-card"></i>
+                    <span>Tentang Kita</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link"><i class="fas fa-home fa-2x" id="home-icon"></i></a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link"><i class="fas fa-envelope-open-text fa-2x" id="home-icon"></i></a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Thesis Universitas Lambung Mangkurat</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <script src="{{asset('assets/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('assets/js/sb-admin-2.min.js') }}"></script>
+
+    <!-- Page level plugins -->
+    <script src="{{asset('assets/vendor/chart.js/Chart.min.js') }}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{asset('assets/js/demo/chart-area-demo.js') }}"></script>
+    <script src="{{asset('assets/js/demo/chart-pie-demo.js') }}"></script>
+
+    @yield('scriptbawah')
+</body>
+
+</html>
